2dc1cfa18c7b3b8523627bd00b5cd80743daacdc
amolpalwe2002 <amolpalwe2002@gmail.com>
2024-06-08 15:00:48 +0530

    add layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,16 +6,48 @@ import {
   SignedOut,
   UserButton
 } from '@clerk/nextjs'
+
+import { Inter, Space_Grotesk as spaceGrotesk } from 'next/font/google'
+import type { Metadata } from 'next';
 import './globals.css'
+
+const inter = Inter({
+  subsets: ['latin'],
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+  variable: '--font-inter'
+})
+
+const spaceGrot= spaceGrotesk({
+  subsets: ['latin'],
+  weight: ['300', '400', '500', '600', '700'],
+  variable: '--font-spaceGrotesk'
+})
+
+
+export const metadata: Metadata = {
+  title: 'DevFlow',
+  description: 'A community-driven platform for asking and answering programming questions. Get help, share knowledge, and collaborate with developers from around the world. Explore topics in web development, mobile app development, algorithms, data structures, and more.',
+  icons: {
+    icon: '/assets/images/site-logo.svg'
+  }
+}
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+    appearance={{
+      elements: {
+        formButtonPrimary: 'primary-gradient',
+        footerActionLink: 'primary-text-gradient hover:text-primary-500'
+      }
+    }}
+    >
       <html lang="en">
-        <body>
+        <body className={`${inter.variable} ${spaceGrot.variable}`}>
           <SignedOut>
             <SignInButton />
           </SignedOut>
